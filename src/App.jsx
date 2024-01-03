c008860e6fdf91facb71d16537346105a98329fd
Arslan Javaid <aja118379@gmail.com>
2024-01-03 14:44:52 +0500

    Basic website completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,39 @@
 import { useState } from 'react'
 
 function App() {
+
+  const [color, setColor] = useState('black')
   
   return (
-    <div className='bg-red-800 mt-5 h-full w-full'>
-      <h1 className='bg-red-300'>Checking Git</h1>
+    <div className='h-screen w-full flex flex-col items-center justify-between' style={{ backgroundColor: color }}>
+      <h1 className='text-white mt-20 text-4xl'>Background Color Changing Website</h1>
+      <div className='bg-white w-9/12 rounded-3xl mb-6 flex justify-center'>
+        <button 
+          className='bg-red-500 rounded-2xl ml-3 my-2 text-white py-0.5 px-5'
+          onClick={() => setColor("red")}>
+          Red
+        </button>
+        <button 
+          className='bg-green-600 rounded-2xl ml-3 my-2 text-white py-0.5 px-5'
+          onClick={() => setColor("rgb(22 163 74)")}>
+          Green
+        </button>
+        <button 
+          className='bg-orange-500 rounded-2xl ml-3 my-2 text-white py-0.5 px-5'
+          onClick={() => setColor("orange")}>Orange</button>
+        <button 
+          className='bg-blue-500 rounded-2xl ml-3 my-2 text-white py-0.5 px-5'
+          onClick={() => setColor("rgb(96 165 250)")}>Blue</button>
+        <button 
+          className='bg-yellow-500 rounded-2xl ml-3 my-2 text-white py-0.5 px-5'
+          onClick={() => setColor("rgb(234 179 8)")}>Yellow</button>
+        <button 
+          className='bg-slate-600 rounded-2xl ml-3 my-2 text-white py-0.5 px-5'
+          onClick={() => setColor("rgb(71 85 105)")}>Slate</button>
+        <button 
+          className='bg-stone-600 rounded-2xl ml-3 my-2 text-white py-0.5 px-5'
+          onClick={() => setColor("rgb(87 83 78)")}>Stone</button>
+      </div>
     </div>
   )
 }
