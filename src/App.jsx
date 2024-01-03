8904d78ce19d749c87d0143f34c5a05bcd6f13c1
Arslan Javaid <aja118379@gmail.com>
2024-01-03 13:49:14 +0500

    First Commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+
+function App() {
+  
+  return (
+    <div className='bg-red-800 mt-5 h-full w-full'>
+      <h1 className='bg-red-300'>Hello 2</h1>
+    </div>
+  )
+}
+
+export default App
