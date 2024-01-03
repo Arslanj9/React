2ce0ab0ddead63599ad837591956b5974f741724
Arslan Javaid <aja118379@gmail.com>
2024-01-03 13:51:47 +0500

    Second Commit

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ function App() {
   
   return (
     <div className='bg-red-800 mt-5 h-full w-full'>
-      <h1 className='bg-red-300'>Hello 2</h1>
+      <h1 className='bg-red-300'>Checking Git</h1>
     </div>
   )
 }
